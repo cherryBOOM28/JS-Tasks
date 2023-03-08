2b8847106816188f89cc1cdfb957affc1fd16709
Louisa Sapina <62461105+LouisaSapina@users.noreply.github.com>
2023-03-09 01:11:23 +0600

    7. Events

--- 6.js
+++ 6.js
@@ -18,11 +18,11 @@ inputTag.addEventListener("keydown", (event) => {
 // Вставить в html тег input (просто предусмотреть в разметке).
 // Обрабатывая событие keyup на теге input, выводить в консоль введенный текст каждый раз, как только клиент вписывает любой символ в поле (или стирает любой символ из поля). Вам понадобится считывать значение поля, это input.value
 
-// const taskTwoInput = document.querySelector(".taskTwoInput");
+const taskTwoInput = document.querySelector(".taskTwoInput");
 
-// taskTwoInput.addEventListener("keyup", (event) => {
-//     console.log(event.target.value);
-// })
+taskTwoInput.addEventListener("keyup", (event) => {
+    console.log(event.target.value);
+})
 
 
 // Задание 3
