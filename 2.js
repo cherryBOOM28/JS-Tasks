c37be705af0ad6911d08c29a109a57f2183db301
Louisa Sapina <62461105+LouisaSapina@users.noreply.github.com>
2023-03-10 13:36:44 +0600

    7. Cycles and functions

--- 2.js
+++ 2.js
@@ -22,7 +22,7 @@ console.log(num);
 // - проверять, равно ли это число 10. Если равно, выводить "Равно 10". Иначе выводить "Не равно 10"
 
 let result;
-for (i = 0; i <= 8; i=i+2) {
+for (i = 0; i <= 8; i = i + 2) {
     let userMessage = prompt("Введите число");
     result = (userMessage == 10) ? "Равно 10" : "Не равно 10";
 }
@@ -33,10 +33,8 @@ console.log(result);
 // Вывести в консоль квадраты чисел от 0 до 100 (не включая 100) - т.е. вывести числа 0, 1, 4, 9, 16...
 
 let answer = prompt("Сколько вы хотите: ");
-console.log(0);
-console.log(1);
-for (i = 2; i < Number(answer); i = i ** 2) {
-    console.log(i);
+    for (let i = 0; i < Number(answer); i++) {
+        console.log(i ** 2);
 }
 
 
@@ -44,18 +42,24 @@ for (i = 2; i < Number(answer); i = i ** 2) {
 //Напишите программу, которая выводит в консоль числа от 1 до 100. При этом вместо чисел, кратных трем, программа должна выводить слово «Fizz», а вместо чисел, кратных пяти, — слово «Buzz». Если число кратно и 3, и 5, то программа должна выводить слово «FizzBuzz»
 
 for (i = 1; i <= 100; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz: " + i)
+    }
     if (i % 3 == 0) {
         console.log("Fizz: " + i);
     } else if (i % 5 == 0) {
         console.log("Buzz: " + i);
     } else  {
-        console.log("FizzBuzz: " + i);
+        console.log(i + " " + "не делится на 5 и на 3");
     }
 }
 
 // Через тернарный оператор
 for (i = 1; i <= 100; i++) {
-    i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i;
+    i % 3 == 0 && i % 5 == 0 
+    ? "FizzBuzz" : i % 3 == 0 
+    ? "Fizz" : i % 5 == 0 
+    ? "Buzz" : i;
 }
 
 // Задание 6
@@ -68,13 +72,15 @@ while (i < 3) {
 // Задание 7
 // Переписать задание 4 в виде функции. Т.е. описать функцию, которая выводит квадраты чисел из заданного диапазона (диапазон будет попадать в функцию как аргументы).
 
-let number = prompt("Введите число: ");
-function squareOfNumbers(answer) {
-    for (i = 0; i < number; i++) {
+let number = prompt("Введите начальное число: ");
+let number2 = prompt("Введите конечное число: ");
+
+function squareOfNumbers(initial, final) {
+    for (i = initial; i < final; i++) {
         console.log(i ** 2);
     }
 }
-squareOfNumbers(10);
+squareOfNumbers(number, number2);
 
 
 // Задание 8
@@ -102,10 +108,10 @@ getRandomRGB(red, green, blue);
 // Такая функция не будет ничего возвращать, она просто выводит информацию в консоль.
 
 function cycle(n) {
-    for (let i = 1; i < n; i = i + 0.5) {
-        if(Number.isInteger(n) === false) {
-            console.log("Ошибка, " + n + "не целое число");
-        } else {
+    if(Number.isInteger(n) === false) {
+        console.log("Ошибка, " + n + "не целое число");
+    } else {
+        for (let i = 1; i < n; i = i + 0.5) {
             if(Number.isInteger(i)) {
                 console.log("Целое число: " + i);
             }
