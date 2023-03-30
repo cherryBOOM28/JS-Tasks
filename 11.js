4a4b490950fd21d82c4a3f603e0dbd6fdd336dc1
Louisa Sapina <62461105+LouisaSapina@users.noreply.github.com>
2023-03-30 18:31:16 +0600

    11. Advanced work with objects and arrays

--- 11.js
+++ 11.js
@@ -2,31 +2,31 @@
 
 // 1.1 Задать массив имен. Создать 2 переменные, которые будут хранить первых 2 имени, вывести их в консоль. Создать массив, который будет хранить остальные имена, вывести имена из него по одному в консоль. Если кол-во элементов массива меньше 2, то вторая переменная должна стать равна "alex".
 
-let names = [
+let names1 = [
     "Sam",
     // "Rose",
 ];
 
+let firstName = names1[0];
+let secondName = names1[1];
+console.log(firstName + ", " + secondName);
+
 let names2 = [
-    ...names,
+    ...names1,
     "Tom",
     "John",
     "Sofia",
 ];
 
-let firstName = names[0];
-let secondName = names[1];
-console.log(firstName + ", " + secondName);
+if (2 > names1.length) {
+    names1.push("alex");
+};
+console.log(names1);
 
 // console.log(names2)
-names2.forEach(names2 => {
-    console.log(names2)
+names2.forEach(name => {
+    console.log(name)
 });
-
-for (let i = 0; i < names.length; i++) {
-    names[1] = "alex"
-};
-console.log(names);
 
 
 // 1.2 Задать массив имен. Задать функцию, которая принимает 3 параметра и выводит их в консоль. Передать массив в функцию. Внутри функции каждый параметр должен быть равен одному из элементов массива
@@ -43,10 +43,19 @@ nameFunc(names);
 
 // 1.3 Задать объект с N полей. Создать 2 переменные, которые будут называться так же, как поля, и хранить 2 значения полей объекта. Если соответствующих полей в объекте нет, задать значения чисел 1 и 2. Все остальные поля объекта записать в переменную fields.
 
-let obj = {
-    field1: "first field",
-    field2: "second field",
-    field3: "third field",
-    field4: "forth field",
+
+let city = {
+    city1: "Astana",
+    city2: "Almaty",
+    city3: "Shymkent",
 };
+
+let {city1, city2, city3, ...fields} = city;
+
+for(let value in city) {
+    console.log(city[value]);
+}
+
+console.log(city1 + ", " + city2 + ", " +  city3 + ", " +  fields);
+
 
